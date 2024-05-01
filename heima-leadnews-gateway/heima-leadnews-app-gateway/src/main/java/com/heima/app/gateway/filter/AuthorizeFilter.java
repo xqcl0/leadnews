3ade3eb102f29da1b550ec3bbb95c53dd7278025
xqcl0 <2694956143@qq.com>
2024-05-01 21:35:04 +0800

    app网关搭建

---
+++ heima-leadnews-gateway/heima-leadnews-app-gateway/src/main/java/com/heima/app/gateway/filter/AuthorizeFilter.java
@@ -0,0 +1,56 @@
+package com.heima.app.gateway.filter;
+
+import com.heima.app.gateway.util.AppJwtUtil;
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class AuthorizeFilter implements Ordered, GlobalFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        //1.拿到request和response
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        //2.判断是否是登录
+        if (request.getURI().getPath().contains("/login")) {
+            return chain.filter(exchange);
+        }
+        //3.获取token
+        String token = request.getHeaders().getFirst("token");
+        //4.验证token是否存在
+        if (StringUtils.isBlank(token)) {
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+        //5。验证token是否有效
+        try {
+            Claims claimsBody = AppJwtUtil.getClaimsBody(token);
+            int result = AppJwtUtil.verifyToken(claimsBody);
+            if (result == 1 || result == 2) {
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                return response.setComplete();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
