4fd15b7010ad9a922a6757e49c7c0eeefb484316
xqcl0 <2694956143@qq.com>
2024-05-11 22:24:48 +0800

    MinIO封装成starter，自媒体网关与微服务搭建

---
+++ heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
@@ -0,0 +1,49 @@
+package com.heima.minio.test;
+
+import com.heima.file.service.FileStorageService;
+import com.heima.minio.MinIOApplication;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@SpringBootTest(classes = MinIOApplication.class)
+@RunWith(SpringRunner.class)
+public class MinIOTest {
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @Test
+    public void test() throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("D:\\start.html");
+        String path = fileStorageService.uploadHtmlFile("", "hello.html", fileInputStream);
+        System.out.println(path);
+    }
+
+//    public static void main(String[] args) {
+//        try {
+//            FileInputStream fileInputStream = new FileInputStream("D:\\start.html");
+//            MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123")
+//                    .endpoint("http://192.168.118.128:9000")
+//                    .build();
+//            PutObjectArgs putObjectArgs = PutObjectArgs
+//                    .builder().bucket("leadnews")
+//                    .object("start.html")
+//                    .contentType("text/html")
+//                    .stream(fileInputStream, fileInputStream.available(), -1).build();
+//            minioClient.putObject(putObjectArgs);
+//
+//            System.out.println("http://192.168.118.128:9000/leadnews/start.html");
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//    }
+}
