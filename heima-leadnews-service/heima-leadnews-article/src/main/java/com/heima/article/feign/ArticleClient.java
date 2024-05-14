5aa046c2201c0163684d81c40b25cebde837dc9a
xqcl0 <2694956143@qq.com>
2024-05-14 21:18:27 +0800

    阿里云安全检测接口，文章保存微服务feign接口调用业务开发

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/feign/ArticleClient.java
@@ -0,0 +1,18 @@
+package com.heima.article.feign;
+
+import com.heima.apis.article.IArticleClient;
+import com.heima.article.service.ApArticleService;
+import com.heima.model.article.dtos.ArticleDto;
+import com.heima.model.common.dtos.ResponseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArticleClient implements IArticleClient {
+    @Autowired
+    private ApArticleService apArticleService;
+    @Override
+    public ResponseResult saveArticle(ArticleDto dto) {
+        return apArticleService.saveArticle(dto);
+    }
+}
